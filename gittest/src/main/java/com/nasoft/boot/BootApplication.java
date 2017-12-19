81050fa7dc122ba7b415214916aef04b291906f6
wangweili <1211840085@qq.com>
2017-12-19 13:30:52 +0800

    delete file

--- gittest/src/main/java/com/nasoft/boot/BootApplication.java
+++
@@ -1,8 +0,0 @@
-package com.nasoft.boot;
-
-public class BootApplication {
-
-	public static void main(String[] args){
-		
-	}
-}
--- gittest/src/main/java/com/nasoft/boot/Test.java
+++
@@ -1,5 +0,0 @@
-package com.nasoft.boot;
-
-public class Test {
-
-}
